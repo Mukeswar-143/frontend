c14b6871a00413aca02b725231d41b31d527328d
Mukeswar <vasanamukeswar@gmail.com>
2025-06-09 13:03:52 -0700

    fetch added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
 
 function App() {
+
+  useEffect(()=>{
+    fetch('http://localhost:8080/Student/hello')
+    .then(Response=>Response.text())
+    .then(result=>console.log(result));
+  },[]);
   return (
     <div className="App">
      hi to all
